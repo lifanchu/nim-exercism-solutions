1deadda34cf5279b6181656745a3ba7e37721550
root <charles@boxofmanga.com>
2020-11-30 19:13:22 +0900

    Faster acronym solution.

--- acronym/acronym.nim
+++ acronym/acronym.nim
@@ -1,6 +1,15 @@
 import nre, strutils
 
 proc abbreviate*(name: string): string =
-  for found in name.toUpperAscii.findIter(re"([A-Z])[A-Z']*"):
-    result.add found.captures[0]
+  var firstCharAdded = false
+  for c in name.toUpperAscii:
+    if c.isAlphaAscii and not firstCharAdded:
+      result.add $c
+      firstCharAdded = true
+    elif not c.isAlphaAscii and not (c == '\''):
+      firstCharAdded = false 
+    
+# proc abbreviate*(name: string): string =
+#   for found in name.toUpperAscii.findIter(re"([A-Z])[A-Z']*"):
+#     result.add found.captures[0]
   
