0df173bdb4dcc533cfc26fdb64ab7b308e513ce7
root <charles@boxofmanga.com>
2020-12-03 18:31:06 +0900

    Moved graphic to code block

--- armstrong-numbers/armstrong_numbers.nim
+++ armstrong-numbers/armstrong_numbers.nim
@@ -1,9 +1,10 @@
 
-import strutils, sequtils, math
+import strutils, sequtils, math, sugar
 
 proc grow(num: int): int =
-    var digits = num.intToStr.map(proc(x: char): int = int(x)-int('0'))
-    digits = digits.map(proc(x: int): int = x ^ digits.len )
+    # var digits = num.intToStr.map(proc(x: char): int = int(x)-int('0'))
+    var digits = num.intToStr.mapIt(int(it) - int('0')).mapIt(it * 2)
+    digits = digits.map(proc(x: int): int = x ^ digits.len)
     result = digits.foldl(a+b)
     
 proc isArmstrongNumber*(num: int): bool =
